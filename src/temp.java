7f1810d0a7145212363b15f36123d10cbbb5cfae
Richard Jansen <richard@lrtbl.nl>
2015-09-24 12:51:53 +0200

    poepkljhkl

--- src/temp.java
+++ src/temp.java
@@ -2,7 +2,7 @@
  * Created by richard on 9/24/15.
  */
 public class temp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Tempest");
     }
 }
